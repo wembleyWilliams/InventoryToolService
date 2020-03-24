63784a8a10d8c5fc96e1b3c7b104962892846a18
wembleyWilliams <58795203+wembleyWilliams@users.noreply.github.com>
2020-03-24 00:53:55 -0500

    Add files via upload
    Working timer and log creation.

---
+++ AuditService.cs
@@ -0,0 +1,150 @@
+﻿using System.IO;
+using System.Threading;
+using System.Configuration;
+using System.ServiceProcess;
+using System;
+
+
+namespace InventoryAuditService
+{
+    public partial class AuditService : ServiceBase
+    {        
+        public AuditService()
+        {
+            InitializeComponent();
+        }
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Audit Service Started");
+            this.ScheduleService();
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Audit Service stopped {0}");
+            this.Schedular.Dispose();
+        }
+
+        private Timer Schedular;
+
+        public void ScheduleService()
+        {
+            try
+            {
+                
+                Schedular = new Timer(new TimerCallback(SchedularCallback));
+                string mode = ConfigurationManager.AppSettings["Mode"].ToUpper();
+                WriteToFile("Audit Service Mode: " + mode + " {0}");
+
+                //Set the Default Time.
+                DateTime scheduledTime = DateTime.MinValue;
+
+                if (mode == "DAILY")
+                {
+                    //Get the Scheduled Time from AppSettings.
+                    scheduledTime = DateTime.Parse(ConfigurationManager.AppSettings["ScheduledTime"]);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next day.
+                        scheduledTime = scheduledTime.AddDays(1);
+                    }
+                }
+
+                if (mode.ToUpper() == "INTERVAL")
+                {
+                    //Get the Interval in Minutes from AppSettings.
+                    int intervalMinutes = Convert.ToInt32(ConfigurationManager.AppSettings["IntervalMinutes"]);
+
+                    //Set the Scheduled Time by adding the Interval to Current Time.
+                    scheduledTime = DateTime.Now.AddMinutes(intervalMinutes);
+                    if (DateTime.Now > scheduledTime)
+                    {
+                        //If Scheduled Time is passed set Schedule for the next Interval.
+                        scheduledTime = scheduledTime.AddMinutes(intervalMinutes);
+                    }
+                }
+
+                TimeSpan timeSpan = scheduledTime.Subtract(DateTime.Now);
+                string schedule = string.Format("{0} day(s) {1} hour(s) {2} minute(s) {3} seconds(s)", timeSpan.Days, timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+
+                WriteToFile("Audit Service scheduled to run after: " + schedule + " {0}");
+
+                //Get the difference in Minutes between the Scheduled and Current Time.
+                int dueTime = Convert.ToInt32(timeSpan.TotalMilliseconds);
+
+                //Change the Timer's Due Time.
+                Schedular.Change(dueTime, Timeout.Infinite);
+            }
+            catch (Exception ex)
+            {
+                WriteToFile("Audit Service Error on: {0} " + ex.Message + ex.StackTrace);
+
+                //Stop the Windows Service.
+                using (ServiceController serviceController = new ServiceController("InventoryService"))
+                {
+                    serviceController.Stop();
+                }
+            }
+        }
+
+        private void SchedularCallback(object e)
+        {
+            this.WriteToFile("Audit Service Log: {0}");
+            this.ScheduleService();
+        }
+
+        private void WriteToFile(string text)
+        {            
+            string path = @"C:\Audit.txt";
+            try
+            {
+                if(!File.Exists(path))
+                { //Create a file to write to
+                    using (StreamWriter writer = File.CreateText(path))
+                    {
+                        File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Hidden);
+                        writer.WriteLine(string.Format(text + "  --   {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                        writer.Close();
+                    }
+                } else
+                {
+                    using (StreamWriter writer = File.AppendText(path))
+                    {
+                        writer.WriteLine(string.Format(text + "  --   {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                        writer.Close();
+                    }
+                }                               
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        private void WriteToFile(string text, string path)
+        {
+            try
+            {
+                if (!File.Exists(path))
+                { //Create a file to write to
+                    using (StreamWriter writer = File.CreateText(path))
+                    {
+                        File.SetAttributes(path, File.GetAttributes(path) | FileAttributes.Hidden);
+                        writer.WriteLine(string.Format(text + "  --   {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+
+                    }
+                }
+                else
+                {
+                    using (StreamWriter writer = File.AppendText(path))
+                    {
+                        writer.WriteLine(string.Format(text + "  --   {0}", DateTime.Now.ToString("dd/MM/yyyy hh:mm:ss tt")));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
